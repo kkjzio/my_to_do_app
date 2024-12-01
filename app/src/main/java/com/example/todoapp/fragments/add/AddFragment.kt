ea67733989985dd04fc133734f6e444c2b427d64
kkjz <253839323@sohu.com>
2024-12-01 21:17:39 +0800

    [build] build the nav

---
+++ app/src/main/java/com/example/todoapp/fragments/add/AddFragment.kt
@@ -0,0 +1,23 @@
+package com.example.todoapp.fragments.add
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todoapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AddFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AddFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -0,0 +1,25 @@
+package com.example.todoapp.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todoapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+}
